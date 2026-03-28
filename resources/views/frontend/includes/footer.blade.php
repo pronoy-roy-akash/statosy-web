923e7a9f4da4c390d8d21c6163cf26e45f8a4bfd
Pronoy Roy Akash <pronoyroyakash@gmail.com>
2026-03-29 00:53:49 +0600

    Update social media URLs in footer

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -93,19 +93,23 @@
 
 
             <div class="footer__social">
-                <a class="footer__socialBtn" href="#" aria-label="LinkedIn">
+                <a class="footer__socialBtn" href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"
+                    aria-label="LinkedIn">
                     <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                         <path
                             d="M20.447 20.452H17.09v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.745V9h3.222v1.561h.046c.449-.85 1.546-1.748 3.182-1.748 3.404 0 4.032 2.241 4.032 5.156v6.483zM5.337 7.433A1.998 1.998 0 1 1 5.34 3.44a1.998 1.998 0 0 1-.003 3.993zM6.956 20.452H3.713V9h3.243v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.727v20.545C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.273V1.727C24 .774 23.2 0 22.222 0h.003z" />
                     </svg>
                 </a>
-                <a class="footer__socialBtn" href="#" aria-label="Facebook">
+                <a class="footer__socialBtn" href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"
+                    aria-label="Facebook">
                     <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                         <path
                             d="M13.5 22v-8h2.7l.4-3h-3.1V9.1c0-.9.3-1.6 1.6-1.6h1.7V4.8c-.3 0-1.4-.1-2.6-.1-2.6 0-4.4 1.6-4.4 4.5V11H7v3h2.7v8h3.8z" />
                     </svg>
                 </a>
-                <a class="footer__socialBtn" href="mailto:hello@statosy.com" aria-label="Email">
+                <a class="footer__socialBtn"
+                    href="https://mail.google.com/mail/?view=cm&fs=1&to=info@statosy.com&su=Hello%20Statosy"
+                    target="_blank" rel="noopener noreferrer" aria-label="Email">
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" aria-hidden="true">
                         <path
                             d="M5 7H19C20.1 7 21 7.9 21 9V17C21 18.1 20.1 19 19 19H5C3.9 19 3 18.1 3 17V9C3 7.9 3.9 7 5 7Z"
